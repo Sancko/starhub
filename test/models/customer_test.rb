e61d1af830c637400a6306a9fef863a0c02cc919
Sancko <pohonets.oleksandr@gmail.com>
2019-01-21 19:57:29 +0200

    test customer model

--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
-class CustomerTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
+describe Customer do
+  # before do
+  #   @bob = customers(:bob)
   # end
+
+  let(:bob) {customers(:bob)}
+  let(:bil) {customers(:bil)}
+  let(:jan) {customers(:jan)}
+
+  describe 'validation' do
+    it 'is valid customer'do
+      expect(bob).must_be :valid?
+    end
+    it 'has already be taken' do
+      expect(bil).must_be :invalid?
+      expect(bil.errors[:username]).must_include "has already been taken"
+    end
+
+    it 'can’t be blank' do
+      expect(jan).must_be :invalid?
+      expect(jan.errors[:last_name]).must_include "can't be blank"
+    end
+  end
 end
