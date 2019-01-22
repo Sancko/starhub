50f610c7023c6939d45116639529d681b5df918d
Sancko <pohonets.oleksandr@gmail.com>
2019-01-22 20:47:00 +0200

    generate new models and write their associations

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -5,4 +5,9 @@ class Customer < ApplicationRecord
          :recoverable, :rememberable, :validatable
   validates :last_name, presence:true
   validates :username, uniqueness:true
+
+  has_many :reviews
+  has_many :issues
+  has_many :product_reviews, through: :reviews, source: :product
+  has_many :problem_products, through: :issues, source: :product
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Customer.delete_all
+Issue.delete_all
+Product.delete_all
+Review.delete_all
+
+# customers
+fred = Customer.create(first_name: 'fred', last_name: 'Hans', username:'fred12', email: 'fred12@gmail.com',
+                       password:'password')
+ted = Customer.create(first_name: 'ted', last_name: 'Hans', username:'ted13', email: 'ted2@gmail.com',
+                       password:'password')
+bob = Customer.create(first_name: 'bob', last_name: 'Hans', username:'bob1232', email: 'bob@gmail.com',
+                       password:'password')
+
+# products
+soap = Product.create(name:'Soap', code:'1234fd', category:'home')
+table = Product.create(name:'Desck', code:'dd-12-4', category:'Furniture')
+food = Product.create(name:'food', code:'1243-asd', category:'food')
+
+# review
+Review.create(customer: fred, product:soap, rating:4, comment:'That was good')
+Review.create(customer: ted, product:table, rating:3, comment:'That was awful')
+Review.create(customer: bob, product:table, rating:5, comment:'That was great')
+
+#issues
+Issue.create(customer: fred, product:food, severity: 'COSMETIC', comment:'Inappropriate condition')
+Issue.create(customer: bob, product:table, severity: 'MINOR', comment:'Left some part')
