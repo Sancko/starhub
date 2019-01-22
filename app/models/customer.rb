b92574ba6c7a6be958962988692c898b4d9aee69
Sancko <pohonets.oleksandr@gmail.com>
2019-01-22 08:23:21 +0200

    add and customize devise gem

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,8 @@
 class Customer < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   validates :last_name, presence:true
   validates :username, uniqueness:true
 end
