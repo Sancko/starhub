8ca5fcbcd309c1fa5bbf6dd3a5ee24f4117791f1
Sancko <pohonets.oleksandr@gmail.com>
2019-01-26 11:39:11 +0200

    set routing to the app

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   devise_for :customers, controllers: { registrations: 'registrations' }
   root to: "home#index"
 
+  resources :products do
+    resources :issues
+    resources :reviews
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
